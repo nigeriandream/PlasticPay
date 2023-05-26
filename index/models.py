e1dd00949a80261a3a0e1b5ef199c17975eceed0
Etette Etok <etokoffice@gmail.com>
2023-05-26 22:33:02 +0100

    fixed models err

--- index/models.py
+++ index/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import shortuuid
-from django.db.models.signal import pre_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 # Create your models here.
@@ -27,12 +27,12 @@ class User(models.Model):
     def __str__(self):
         return self.username
     
-    @receiver(pre_save, sender=User)
-    def generate_unique_number(sender, instance, **kwargs):
-        #only generate random number if it doesn't exist
-        if not instance.unique_number:
-            unique_number = shortuuid.uuid()[:12]
-            instance.unique_number = unique_number
+@receiver(pre_save, sender=User)
+def generate_unique_number(sender, instance, **kwargs):
+    #only generate random number if it doesn't exist
+    if not instance.unique_number:
+        unique_number = shortuuid.uuid()[:12]
+        instance.unique_number = unique_number
 
 # register the signal receiver function
 pre_save.connect(generate_unique_number, sender=User)
